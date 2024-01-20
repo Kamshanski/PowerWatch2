582b0130c3ba9d90fa570ab82a0e39dfc9452d66
ruban <e.ruban@cft.ru>
2024-01-20 15:21:15 +0700

    Добавил тест для проверки функции

---
+++ shared-power-native/src/windowsTest/kotlin/dev/kamshanski/powerwatch2/power/Test.kt
@@ -0,0 +1,16 @@
+package dev.kamshanski.powerwatch2.power
+
+import dev.kamshanski.powerwatch2.model.PowerStatus
+import dev.kamshanski.powerwatch2.power.getPowerStatus
+import kotlin.test.Test
+import kotlin.test.assertIs
+
+class Test {
+
+	@Test
+	fun get_power_status_on_Windows_EXPECT_PowerStatus_Autonomous() {
+		val ps = getPowerStatus()
+
+		assertIs<PowerStatus.Autonomous>(ps)
+	}
+}
